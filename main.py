f4d14aed5b58c1fc58fc260054baffa3ee8c50d0
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-09-19 14:35:07 +0200

    done

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
-def calculate():
-    """
-    calculates the total
-    :return: None
-    """
-    price = 15.75
-    quantity = 3  # TODO change quantity to 5
-    print(f'Total: {price * quantity}')
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+# Eine einfache Datenstruktur zur Speicherung der Blog-Beiträge
+posts = [
+    {'id': 1, 'title': 'Erster Beitrag', 'content': 'Dies ist der Inhalt des ersten Beitrags.'},
+    {'id': 2, 'title': 'Zweiter Beitrag', 'content': 'Dies ist der Inhalt des zweiten Beitrags.'}
+]
+
+
+def blog_overview():
+    """Gibt eine Übersicht aller Blog-Beiträge als JSON zurück."""
+    pass
+
+
+def blog_detail(post_id):
+    """Gibt die Details eines bestimmten Blog-Beitrags als JSON zurück."""
+    pass
+
+
+def add_post():
+    """Fügt einen neuen Blog-Beitrag hinzu und gibt diesen als JSON zurück."""
+    pass
+
+
+def delete_post(post_id):
+    """Löscht einen bestimmten Blog-Beitrag und gibt eine Bestätigungsnachricht zurück."""
+    pass
 
 
 if __name__ == '__main__':
-    calculate()
+    app.run()
--- main_test.py
+++ main_test.py
@@ -1,6 +1,35 @@
-import main
+import pytest
+from main import app, posts
 
-def test(capsys):
-    main.calculate()
-    captured = capsys.readouterr()
-    assert captured.out == "Total: 78.75\n"
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+
+def test_blog_overview(client):
+    response = client.get('/posts')
+    assert response.status_code == 200
+    assert len(response.get_json()) == 2
+
+
+def test_blog_detail(client):
+    response = client.get('/posts/1')
+    assert response.status_code == 200
+    assert response.get_json()['title'] == 'Erster Beitrag'
+
+
+def test_add_post(client):
+    new_post = {'title': 'Dritter Beitrag', 'content': 'Dies ist der Inhalt des dritten Beitrags.'}
+    response = client.post('/posts', json=new_post)
+    assert response.status_code == 201
+    assert response.get_json()['title'] == new_post['title']
+
+
+def test_delete_post(client):
+    response = client.delete('/posts/1')
+    assert response.status_code == 200
+    assert response.get_json()['message'] == 'Post erfolgreich gelöscht'
+    assert len(posts) == 1
