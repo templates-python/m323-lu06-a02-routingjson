3f3df74b3a5ae1a73f7542eb6971860d5606af9c
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-09-19 14:52:20 +0200

    ausformuliert

--- main.py
+++ main.py
@@ -9,21 +9,25 @@ posts = [
 ]
 
 
+# TODO: Implementiere eine Route für '/posts', die bei einem GET-Request eine Übersicht aller Blog-Beiträge als JSON zurückgibt.
 def blog_overview():
     """Gibt eine Übersicht aller Blog-Beiträge als JSON zurück."""
     pass
 
 
+# TODO: Implementiere eine Route für '/posts/<int:post_id>', die bei einem GET-Request die Details eines bestimmten Blog-Beitrags als JSON zurückgibt.
 def blog_detail(post_id):
     """Gibt die Details eines bestimmten Blog-Beitrags als JSON zurück."""
     pass
 
 
+# TODO: Implementiere eine Route für '/posts', die bei einem POST-Request einen neuen Blog-Beitrag hinzufügt und diesen als JSON zurückgibt.
 def add_post():
     """Fügt einen neuen Blog-Beitrag hinzu und gibt diesen als JSON zurück."""
     pass
 
 
+# TODO: Implementiere eine Route für '/posts/<int:post_id>', die bei einem DELETE-Request einen bestimmten Blog-Beitrag löscht und eine Bestätigungsnachricht zurückgibt.
 def delete_post(post_id):
     """Löscht einen bestimmten Blog-Beitrag und gibt eine Bestätigungsnachricht zurück."""
     pass
